1047a6884e8c03c1c974cef6031979aaeccee04d
chimepalden <chimepalden@gmail.com>
2020-09-25 01:28:17 -0400

    imported module for google map and assigned apiKey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule} from '@agm/core';
+// import { GoogleMapsModule } from '@angular/google-maps';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    NavMenuComponent,
+    HomeComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    EducationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // GoogleMapsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAuNre_l4nGqY4gP9SeQD-PyGbfuBzB168'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
