b286ad326fbe9b6cb80184a03649bf2064d1abdd
chimepalden <chimepalden@gmail.com>
2020-09-25 01:45:09 -0400

    configured in-app routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chime',
+    children: [
+      { path: '', component: HomeComponent},
+      { path: 'resume',
+        children: [
+          {path: '', component: ResumeComponent},
+          {path: 'academicprojects', component: EducationComponent},
+          {path: 'courses', component: EducationComponent}
+        ]
+      },
+      { path: 'projects', component: ProjectsComponent},
+      { path: 'contact', component: ContactComponent},
+      { path: 'about', component: AboutComponent},
+    ]
+  },
+  { path: '', redirectTo: 'chime', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
