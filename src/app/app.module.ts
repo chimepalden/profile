46c2733dd863abaa04fdeaad2145142da2627ec1
chimepalden <chimepalden@gmail.com>
2020-10-22 01:26:29 -0400

    new built with updated about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AgmCoreModule} from '@agm/core';
 // import { GoogleMapsModule } from '@angular/google-maps';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
